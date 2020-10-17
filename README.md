60fdc329816b939e37f451d2d6e97bf15eb61761
M. Revuelta <miguel.revuelta@taiger.com>
2020-10-17 17:59:04 +0200

    Default project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pip example package
+
+[Tutorial about how to distribute pip packages](https://packaging.python.org/tutorials/packaging-projects/)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pip-example-pkg-revuel",
+    version="0.1.0",
+    author="revuel",
+    author_email="revuel22@hotmail.com",
+    description="Tutorial to distribute a pip package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/revuel/pip-example-pkg-revule",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
